b77ae39c11b6629e0363ead85a3784d2d5bfa27b
Alexander <rednaxela@tuta.io>
2026-01-17 12:04:32 +0100

    Variables excerse

--- Variables/main.c
+++ Variables/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+// Context
+// int main() {
+//     int a = 0, b = 1, c = 2, d = 3, e = 4;
+//     a = b - c + d * e;
+//     printf("%d\n", a);
+//     return 0;
+// }
+
+//Exercise create a program which prints out the sum of the numbers a, b, and c.
 int main() {
-    int a = 0, b = 1, c = 2, d = 3, e = 4;
-    a = b - c + d * e;
-    printf("%d\n", a);
+    int a = 0, b = 1, c = 2;
+    int result = a + b + c;
+    printf("%d\n", result);
+
     return 0;
 }
