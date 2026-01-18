ae5e263111a2a706b963129a6e38e0a04ca2f128
Alexander <rednaxela@tuta.io>
2026-01-18 19:35:09 +0100

    Clang formatting on save config

--- Conditions/main.c
+++ Conditions/main.c
@@ -1,18 +1,18 @@
 #import <stdio.h>
 
-void guessNumber(int guess) {
+void guessNumber(int guess)
+{
     if (guess < 555) {
         printf("Your guess is to low.\n");
-    }
-    else if (guess == 555) {
-        printf("Correct. Your guessed it!\n");
-    }
-    else {
-    printf("Your guess is to high.\n");
+    } else if (guess == 555) {
+        printf("Correct.Your guessed it!\n");
+    } else {
+        printf("Your guess is to high.\n");
     }
 }
 
-int main() {
+int main()
+{
     guessNumber(500);
     guessNumber(600);
     guessNumber(555);
