e2c0b7e1399ce86c0575387371634a09c1c21b64
Alexander <rednaxela@tuta.io>
2026-01-18 13:34:18 +0100

    Multidimensional Arrays exercise setup

--- Arrays/main.c
+++ Arrays/main.c
@@ -6,10 +6,7 @@
 //     numbers[0] = 10;
 //     numbers[1] = 20;
 //     numbers[2] = 30;
-//     numbers[3] = 40;
-//     numbers[4] = 50;
-//     numbers[5] = 60;
-//     numbers[6] = 70;
+//     numbers[3] = 40; numbers[4] = 50; numbers[5] = 60; numbers[6] = 70;
 
 //     printf("The 7th number in the array is %d\n", numbers[6]);
 //     return 0;
