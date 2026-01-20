a58fa68786101f4263d329269eed87db463536c5
Alexander <rednaxela@tuta.io>
2026-01-20 20:33:09 +0100

    Function Arguments by Reference finished

--- Learn-C.org/FunctionArgsByReference/main.c
+++ Learn-C.org/FunctionArgsByReference/main.c
@@ -1,6 +1,19 @@
 #import <stdio.h>
 
+typedef struct {
+    char *name;
+    int age;
+} person;
+
+typedef struct {
+    int x;
+    int y;
+} point;
+
 void addone(int *n);
+void move(point *p);
+void move2(point *p);
+void birthday(person *p);
 
 int main()
 {
@@ -10,6 +23,15 @@ int main()
     // Pass the reference to n, not the value itself so the function can edit it effectively.
     addone(&n);
     printf("After: %d\n", n);
+
+    // Write a function called birthday, which adds one to the age of a person.
+    person pers;
+    pers.name = "Alexander";
+    pers.age = 32;
+    printf("%d before birthday\n", pers.age);
+    birthday(&pers); // reference person witg & to be able to change the sttruct value itself.
+    printf("%d after birthday\n", pers.age);
+
     return 0;
 }
 
@@ -18,3 +40,17 @@ void addone(int *n)
     // n is a pointer here which point to a memory-adress outside the function scope
     (*n)++; // this will effectively increment the value of n
 }
+
+void move(point *p)
+{
+    (*p).x++;
+    (*p).y++;
+}
+
+void move2(point *p)
+{
+    p->x++;
+    p->y++;
+}
+
+void birthday(person *p) { p->age += 1; }
